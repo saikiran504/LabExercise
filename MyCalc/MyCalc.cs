2e5473cc39ca2ddafcd81f965bd8800f3f7c5585
Sai Kiran Gujjeti <E009146@cigniti.com>
2022-08-17 17:27:04 +0530

    update

---
+++ MyCalc/MyCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LabExercise
+{
+    public class MyCalc
+    {
+        public static double Add(double op1,double op2)
+        {
+            return op1+op2;
+        }
+        public static double Sub(double op1,double op2)
+        {
+            return op1-op2;
+        }
+        public static double Mul(double op1,double op2)
+        {
+            return op1*op2;
+        }
+         public static double Pow(double op1,double op2)
+        {    
+            double power=Math.Pow(op1,op2);
+            return power;
+        }
+    }
+}
---
+++ MyCalcTest/MyCalcTest.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabExercise;
+namespace LabExercise
+{
+    [TestClass]
+    public class MyCalcTest
+    {
+        [DataTestMethod]
+        [DataRow(10,20,30)]
+        public void TestDataAddMethod(double op1,double op2,double op3)
+        {
+
+                double actualResult=MyCalc.Add(op1,op2);
+                Assert.AreEqual(op3,actualResult,"Add Test is Failed ");
+        }
+        [DataTestMethod]
+        [DataRow(10,20,-10)]
+         public void TestDataSubMethod(double op1,double op2,double op3)
+        {
+
+                double actualResult=MyCalc.Sub(op1,op2);
+                Assert.AreEqual(op3,actualResult,"Sub Test is Failed ");
+        }
+        // [DataTestMethod]
+        // [DataRow(10,20,200)]
+        //  public void NegativeTestDataSubMethod(double op1,double op2,double op3)
+        // {
+
+        //         double actualResult=MyCalc.Sub(op1,op2);
+        //         Assert.AreNotEqual(op3,actualResult,"Add Test is Failed ");
+        // }
+        [DataTestMethod]
+        [DataRow(10,20,0.6)]
+         public void TestDataMulMethod(double op1,double op2,double op3)
+        {
+
+                double actualResult=MyCalc.Mul(op1,op2);
+                Assert.AreEqual(op3,actualResult,"Mul Test is Failed ");
+        }
+         [DataTestMethod]
+        [DataRow(10,2,100)]
+         public void TestDataPowMethod(double op1,double op2,double op3)
+        {
+
+                double actualResult=MyCalc.Pow(op1,op2);
+                Assert.AreEqual(op3,actualResult,"Pow Test is Failed ");
+        }
+    }
+}
