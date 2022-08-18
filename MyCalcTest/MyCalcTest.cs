54b8bb313e00a3c773c4d8b2ef97c6435d69af47
saikiran504 <gujjetisaikiran2@gmail.com>
2022-08-18 10:31:06 +0530

    changes done

--- MyCalcTest/MyCalcTest.cs
+++ MyCalcTest/MyCalcTest.cs
@@ -13,11 +13,11 @@ namespace LabExercise
                 double actualResult=MyCalc.Add(op1,op2);
                 Assert.AreEqual(op3,actualResult,"Add Test is Failed ");
         }
+
         [DataTestMethod]
         [DataRow(10,20,-10)]
         [DataRow(10,50,-10)]
         [DataRow(10,60,-10)]
-
          public void TestDataSubMethod(double op1,double op2,double op3)
         {
 
@@ -39,10 +39,10 @@ namespace LabExercise
         {
 
                 double actualResult=MyCalc.Mul(op1,op2);
-                Assert.AreEqual(op3,actualResult,"Mul Test is Failed ");
+                Assert.AreNotEqual(op3,actualResult,"Mul Test is Failed ");
         }
          [DataTestMethod]
-        [DataRow(10,2,100)]
+         [DataRow(10,2,100)]
          public void TestDataPowMethod(double op1,double op2,double op3)
         {
 
